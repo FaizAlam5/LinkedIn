4001daf9e44380c796829b0bc76bc69d50032e9c
FaizAlam5 <faiz.alam@agnext.in>
2024-05-20 18:15:23 +0530

    added ui for messaging

--- src/Pages/Message.jsx
+++ src/Pages/Message.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { auth } from "../firebaseConfig";
 import Loader from "../components/common/Loader";
 
-export default function Home({ currentUser }) {
+export default function Message({ currentUser }) {
   const [loading, setLoading] = useState(true);
   let navigate = useNavigate();
   useEffect(() => {
--- src/components/MessageComponent.jsx
+++ src/components/MessageComponent.jsx
@@ -1,10 +1,26 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "../Sass/MessageComponent.scss";
+import MessageCard from "./common/MessageCard";
 
-export default function RegisterComponent() {
+export default function MessageComponent({currentUser}) {
+    const [modalOpen, setModalOpen]= useState(true);
+    const [isEdit, setIsEdit] = useState(true)
     return (
         <>
-            <p>hi</p>
+            <div>
+                <div className="post-status">
+                    <button
+                        className="open-post-modal"
+                        onClick={() => {
+                            setModalOpen(!modalOpen);
+                            setIsEdit(!isEdit);
+                        }}
+                    >
+                        Start Messaging...
+                    </button>
+                    {modalOpen? <h1></h1>:<MessageCard currentUser={currentUser}/>}
+                </div>
+            </div>
         </>
     );
 }
